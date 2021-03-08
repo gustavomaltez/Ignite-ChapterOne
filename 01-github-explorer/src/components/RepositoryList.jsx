9699070464cfc630f48301349bc4efdf98695c3c
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-08 13:48:18 -0300

    error: preset-typescript error

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
-import { RepositoryItem } from "./RepositoryItem";
+
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
+import { RepositoryItem } from '../components/RepositoryItem';
 
 export function RepositoryList(){
     const [repositories, setRepositories] = useState([]);
@@ -14,7 +15,6 @@ export function RepositoryList(){
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
-
             <ul>
                 {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository}/>)}
             </ul>
